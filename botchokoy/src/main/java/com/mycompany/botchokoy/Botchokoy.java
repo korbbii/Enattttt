53ce1dcf8fc5d637ffccf3b15e0e0ca154b0658c
korbbii <124796384+korbbii@users.noreply.github.com>
2023-11-13 15:24:21 +0800

    Add files via upload

---
+++ botchokoy/src/main/java/com/mycompany/botchokoy/Botchokoy.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.botchokoy;
+
+/**
+ *
+ * @author WS
+ */
+public class Botchokoy {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
